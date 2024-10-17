054e3902504965302fff75b2e485260081dff78c
Ahmad Mostafa <124518625+ahmadalasiri@users.noreply.github.com>
2024-10-17 21:48:00 +0300

    Refactor frontend code to improve readability and add logging

--- frontend/client.js
+++ frontend/client.js
@@ -20,12 +20,17 @@ getData.addEventListener("click", async (event) => {
 
 deleteData.addEventListener("click", async (event) => {
   const name = nameText.value;
+  const category = categoryText.value;
+  const date = dateText.value;
+  const author = authorText.value;
 
   const response = await fetch("http://localhost:4000/deleteData", {
     method: "DELETE",
     headers: { "content-type": "application/json" },
     body: JSON.stringify({ name }),
   });
+
+  console.log(name, category, date, author);
 });
 
 addData.addEventListener("click", async (event) => {
@@ -39,4 +44,6 @@ addData.addEventListener("click", async (event) => {
     headers: { "content-type": "application/json" },
     body: JSON.stringify({ name, category, date, author }),
   });
+
+  console.log(name, category, date, author);
 });
